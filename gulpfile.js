edc598a52416ebc49661c5dce8fda9e1ded3cf55
Jenah <fonsecasantosjean@gmail.com>
2025-04-04 08:10:17 -0300

    projeto completo

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemap = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin')
 const cleancss = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -19,7 +20,14 @@ function images() {
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
